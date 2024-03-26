478c38fb90b6532abed6edc88908cf463a0cefd6
Ludwig3070 <ludovic.vasseur10@gmail.com>
2024-03-26 10:10:42 +0100

    projet terminé pret pour la soutenance

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ import alternativeImg from '../../assets/remplacement.png';
 
 const Card = ({ datas }) => {
     const navigate = useNavigate(); // Utilisation du hook useNavigate pour la navigation
-   
+
 
     /* useEffect numero 1, gere le style à appliquer selon le parent */
 
@@ -28,10 +28,10 @@ const Card = ({ datas }) => {
 
     /* useEffect numero 2, gere l'image à afficher en cas d'erreur d'url ou de reception d'image */
 
-    const [imageLoaded, setImageLoaded] = useState(true);//Etat pour verifier que l'image soit bien chargée
+    const [imageLoaded, setImageLoaded] = useState(false);//Etat pour verifier que l'image soit bien chargée
     useEffect(() => {
         const image = new Image();
-        image.src  = datas?.cover ;
+        image.src = datas?.cover;
 
         // Fonction pour gérer le chargement de l'image
         const handleImageLoad = () => {
@@ -52,37 +52,53 @@ const Card = ({ datas }) => {
             image.onload = null;
             image.onerror = null;
         };
-    }, [datas.cover,imageLoaded]);
+    }, [datas.cover, imageLoaded]);
 
     /* useEffect numero 3, gere l'image à afficher à partir d'ecran superieur à 767px pour stylesHome uniquement */
 
-    useEffect(()=>{
+    useEffect(() => {
         const image = new Image();
         image.src = datas.cover;
-        let displayAlernativeImg = ((style===stylesLogement)&&(window.innerWidth>760 )&&(image.width <(window.innerWidth-90)))&&(image.width <1240)
-        console.log("largeur d image=",image.width);
-        if(displayAlernativeImg){
-            console.log(displayAlernativeImg);            
-            console.log("largeur de fenetre=",window.innerWidth);
-            setImageLoaded(false)        
+        let displayAlernativeImg = ((style === stylesLogement) && (window.innerWidth > 760) && (image.width < (window.innerWidth - 90))) && (image.width < 1240)
+        console.log("largeur d image=", image.width);
+        if (displayAlernativeImg) {
+            console.log(displayAlernativeImg);
+            console.log("largeur de fenetre=", window.innerWidth);
+            setImageLoaded(false)
         }
-    },[datas.cover,imageLoaded])
+    }, [datas.cover, imageLoaded])
+
+    /* useEffect numero 4, gere l'apparition de card */
+
+    const [isVisible, setIsVisible] = useState(false)
+    useEffect(() => { 
+        const timeout = setTimeout(() => {            
+            setIsVisible(true);
+          }, 10); // Délai de 10ms avant d'afficher l'élément      
+          return () => {
+            setIsVisible(false); // Réinitialiser isVisible lorsque le composant est démonté
+            clearTimeout(timeout); // Nettoyer le timeout lors du démontage du composant 
+          }
+    
+    }, [datas])//rendu quand datas change
+   
 
 
-    return (
-        <div className={`${style.cards}`}>{/* La classe CSS 'style.cards_card' est déterminée par 'style', qui est soit stylesLogement soit stylesHome */}
-            <div className={`${style.cards_card}`} onClick={() => datas.id && navigate(`/fichelogement/${datas.id}`)}>{/* Si datas.id existe, on ajoute un événement de clic pour naviguer vers '/fichelogement/${datas.id}' */}
-                <img
-                    src={imageLoaded ? datas.cover : alternativeImg}//affiche l'image alternative en cas d'erreur de chargement de l'image
-                    alt={datas.title || "photos_de_logement"}
-                    className={(datas.title && `${style.cards_card_img_withTextIn}`) || `${style.cards_card_img}`}
 
-                />
-                {/* Si datas.title existe, on affiche un titre */}
-                {datas.title && <h2 className={`${style.cards_card_title}`}>{datas.title}</h2>}
-            </div>
+return (
+    <div className={`${style.cards} ${isVisible ? style.cards_fadeIn: style.cards_fadeOut}`}>{/* La classe CSS 'style.cards_card' est déterminée par 'style', qui est soit stylesLogement soit stylesHome */}
+        <div className={`${style.cards_card} `} onClick={() => datas.id && navigate(`/fichelogement/${datas.id}`)}>{/* Si datas.id existe, on ajoute un événement de clic pour naviguer vers '/fichelogement/${datas.id}' */}
+            <img 
+                src={imageLoaded ? datas.cover : alternativeImg}//affiche l'image alternative en cas d'erreur de chargement de l'image
+                alt={datas.title || "photos_de_logement"}
+                className={(datas.title && `${style.cards_card_img_withTextIn}`) || `${style.cards_card_img}`}
+
+            />
+            {/* Si datas.title existe, on affiche un titre */}
+            {datas.title && <h2 className={`${style.cards_card_title}`}>{datas.title}</h2>}
         </div>
-    );
+    </div>
+);
 };
 
 export default Card;
